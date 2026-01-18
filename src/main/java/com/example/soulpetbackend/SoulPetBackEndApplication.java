e4ef6bf9bd9223223999dabea05528dcdc5017d5
Maris-LTD <letuandat556@gmail.com>
2026-01-18 19:39:35 +0700

    initial commit

---
+++ src/main/java/com/example/soulpetbackend/SoulPetBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.soulpetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulPetBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoulPetBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/soulpetbackend/SoulPetBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soulpetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulPetBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
